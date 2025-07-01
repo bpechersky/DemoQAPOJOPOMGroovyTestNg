56900fa16f2507ab820240afdfbed63d569b5af1
Boris Pechersky <bpechersky@gmx.com>
2025-06-30 20:12:53 -0700

    TestBox in demoqa.com implemented in POJO, POM, Groovy, Rest Assured and Testng

--- src/test/groovy/com/demoqa/pages/TextBoxPage.groovy
+++ src/test/groovy/com/demoqa/pages/TextBoxPage.groovy
@@ -1,32 +1,24 @@
 package com.demoqa.pages
 
-import org.openqa.selenium.By
-import org.openqa.selenium.JavascriptExecutor
-import org.openqa.selenium.WebDriver
+import static com.codeborne.selenide.Selenide.*
+import static com.codeborne.selenide.Selectors.*
+import static com.codeborne.selenide.Condition.*
 
 class TextBoxPage {
-    WebDriver driver
-
-    TextBoxPage(WebDriver driver) {
-        this.driver = driver
-    }
 
     void open() {
-        driver.get("https://demoqa.com/text-box")
+        open("https://demoqa.com/text-box")
     }
 
     void fillForm(String name, String email, String currentAddress, String permanentAddress) {
-        driver.findElement(By.id("userName")).sendKeys(name)
-        driver.findElement(By.id("userEmail")).sendKeys(email)
-        driver.findElement(By.id("currentAddress")).sendKeys(currentAddress)
-        driver.findElement(By.id("permanentAddress")).sendKeys(permanentAddress)
-
-        def submitButton = driver.findElement(By.id("submit"))
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", submitButton)
-        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", submitButton)
+        $("#userName").setValue(name)
+        $("#userEmail").setValue(email)
+        $("#currentAddress").setValue(currentAddress)
+        $("#permanentAddress").setValue(permanentAddress)
+        $("#submit").scrollIntoView(true).click()
     }
 
     String getOutputName() {
-        return driver.findElement(By.id("name")).getText().replace("Name:", "").trim()
+        return $("#name").getText().replace("Name:", "").trim()
     }
 }
--- src/test/groovy/com/demoqa/tests/TextBoxTest.groovy
+++ src/test/groovy/com/demoqa/tests/TextBoxTest.groovy
@@ -1,14 +1,22 @@
 package com.demoqa.tests
 
-import com.demoqa.BaseTest
+import com.codeborne.selenide.Configuration
 import com.demoqa.pages.TextBoxPage
+import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
 
-class TextBoxTest extends BaseTest {
+class TextBoxTest {
+
+    @BeforeClass
+    static void setup() {
+        Configuration.browser = "chrome"
+        Configuration.browserSize = "1920x1080"
+        Configuration.headless = true
+    }
 
     @Test
     void testFillTextBoxForm() {
-        TextBoxPage page = new TextBoxPage(driver)
+        def page = new TextBoxPage()
         page.open()
         page.fillForm("Boris", "boris@example.com", "123 Main St", "456 Other St")
         assert page.getOutputName() == "Boris"
